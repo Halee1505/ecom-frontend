f833f949d6da7aad11c1813692009b3501d7ac76
Nguyễn Hải Linh <medigo.linh.nguyen@gmail.com>
2023-10-11 12:39:07 +0700

    checking product2

--- src/app/san-pham/[slug]/page.tsx
+++ src/app/san-pham/[slug]/page.tsx
@@ -15,7 +15,8 @@ const CategoryPage = async ({ params }: { params: { slug: string } }) => {
       console.error(err);
       return null;
     });
-  console.log(product);
+  console.log("param", params.slug);
+  console.log("product", product);
   if (!product) {
     return <div>Không tìm thấy sản phẩm</div>;
   }
